8f66b5f619e2c3d3a1d781e60972b65e743af179
wafoorks <85328715+wafoorks@users.noreply.github.com>
2021-07-24 21:50:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    introTitle: 'Đan Phượngg Ơiii :))',
+    introDesc: `Anh có một thắc mắc nhỏ
+    Là em ở đâu suốt thời gian qua
+    Còn bây giờ giúp anh một chuyện
+    là trả lời cho anh câu hỏi`,
+    btnIntro: 'hihi ok a :))',
+    title: 'Em có muốn cuộc sống này có thêm anh là người đồng hành chứ😙',
+    desc: 'Phải chăng em đã say ngay từ phút giây đầu tiên ',
+    btnYes: 'Ok anh luôn nè',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    question:'Trên thế giới hơn 7 tỉ người mà anh chỉ muốn đi với mỗi em thôi <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Yêu thì yêu mà không yêu thì cũng phải yêu MUAHAHAH <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Nhanh anh chờ chở đi chơi đấy nhá.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/Trungchieu.810' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
